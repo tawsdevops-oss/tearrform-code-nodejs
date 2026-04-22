297ed1f0522fa9ffbd707dd889d67dc5c1ef4a01
AWS & DevOps <tawsdevops@gmail.com>
2026-04-22 07:12:52 +0530

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,3 @@
 locals {
-  execution_role_arn = var.create_iam_role ?
-    aws_iam_role.ecs_task_execution_role[0].arn :
-    data.aws_iam_role.existing[0].arn
+  execution_role_arn = var.create_iam_role ? aws_iam_role.ecs_task_execution_role[0].arn : data.aws_iam_role.existing[0].arn
 }
