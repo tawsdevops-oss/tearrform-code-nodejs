692498ff5c67fbd0a4c030af1d360f082ad4e62f
tawsdevops-oss <tawsdevops@gmail.com>
2026-04-22 06:43:12 +0530

    ecs chnages

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -27,34 +27,6 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 ########################################
-# IAM Role for ECS Task Execution
-########################################
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.app_name}-ecs-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-########################################
-# Attach AWS Managed Policy
-########################################
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-########################################
 # ECS Task Definition
 ########################################
 resource "aws_ecs_task_definition" "app" {
@@ -64,6 +36,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
 
+  # Reference IAM role from iam.tf
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
